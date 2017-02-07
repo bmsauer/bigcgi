5b60d36f18d4d377150797d1030e6165c382b5a5
Brian Sauer <brianmsauer@gmail.com>
2017-02-06 19:26:51 -0500

    install script

--- serversetup/install.tcl
+++ serversetup/install.tcl
@@ -1,5 +1,36 @@
 #!/usr/local/bin/tclsh8.6
 
+proc safe_append {filename line} {
+    set file_contents [read [open $filename r]]
+    #set idx [string first $line $file_contents]
+    if { [string first $line $file_contents] < 0 } {
+	set fileout [open $filename a]
+	puts $fileout $line
+	close $fileout 
+    }
+}
+
+#---------------------------
+# setup rc.conf
+#---------------------------
+safe_append "/etc/rc.conf" {apache24_enable="yes"}
+safe_append "/etc/rc.conf" {mongod_enable="yes"}
+
+#---------------------------
+# edit sudoers / setup scripts
+#---------------------------
+safe_append "/usr/local/etc/sudoers" {bigcgi ALL=(ALL:ALL) NOPASSWD: /usr/home/bigcgi/bigcgi-repo/script/adduser.tcl}
+safe_append "/usr/local/etc/sudoers" {bigcgi ALL=(ALL:ALL) NOPASSWD: /usr/home/bigcgi/bigcgi-repo/script/moveprog.tcl}
+safe_append "/usr/local/etc/sudoers" {bigcgi ALL=(ALL:ALL) NOPASSWD: /usr/home/bigcgi/bigcgi-repo/script/delprog.tcl}
+
+exec {chmod 700 /usr/home/bigcgi/bigcgi-repo/script/adduser.tcl}
+exec {chmod 700 /usr/home/bigcgi/bigcgi-repo/script/moveprog.tcl}
+exec {chmod 700 /usr/home/bigcgi/bigcgi-repo/script/delprog.tcl}
+
+exec {chown root:root /usr/home/bigcgi/bigcgi-repo/script/adduser.tcl}
+exec {chown root:root /usr/home/bigcgi/bigcgi-repo/script/moveprog.tcl}
+exec {chown root:root /usr/home/bigcgi/bigcgi-repo/script/delprog.tcl}
+
 #---------------------------
 #  apache config
 #---------------------------
@@ -24,6 +55,4 @@ cd ..
 exec python3.5 -m ensurepip
 exec pip3 install virtualenv
 exec virtualenv -p /usr/local/bin/python3.5 env
-
-
 
