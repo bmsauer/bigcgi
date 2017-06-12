516ab13fb13df205c8a2dacaa07c792fbae3b73b
Brian Sauer <brianmsauer@gmail.com>
2017-06-11 21:35:37 -0400

    added license notice to source

--- serversetup/install.tcl
+++ serversetup/install.tcl
@@ -1,5 +1,22 @@
 #!/usr/local/bin/tclsh8.6
 
+
+#This file is part of bigCGI.
+#
+#bigCGI is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#bigCGI is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with bigCGI.  If not, see <http://www.gnu.org/licenses/>.
+
+
 proc safe_append {filename line} {
     set file_contents [read [open $filename r]]
     #set idx [string first $line $file_contents]
