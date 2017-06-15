228216334d0b824a3c273ab6fc3ae18d9981d5bd
Brian Sauer <brianmsauer@gmail.com>
2017-06-15 00:33:50 -0400

    added envinfo file

---
+++ cgi/envinfo.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/bash
+
+echo "Content-Type: text/plain"
+echo ""
+echo ""
+echo "OS:"
+echo $(uname -mrs)
+echo "Bash Interpreter:"
+echo $(which bash)
+echo "Python Interpreter:"
+echo "/usr/local/bin/$(ls /usr/local/bin | grep ^python3..$)"
+echo "/usr/local/bin/$(ls /usr/local/bin | grep ^python2..$)"
+echo "TCL Interpreter:"
+echo "/usr/local/bin/$(ls /usr/local/bin | grep ^tclsh...$)"
+echo "Perl Interpreter:"
+echo $(which perl)
+echo "tcsh Interpreter:"
+echo $(which tcsh)
+echo "AWK Interpreter:"
+echo $(which awk)
