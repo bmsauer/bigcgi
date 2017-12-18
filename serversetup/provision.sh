47bb530b2a8f6c71ce79eeaca20842710b345922
Brian Sauer <brianmsauer@gmail.com>
2017-12-17 20:27:10 -0500

    removing apache

--- serversetup/provision.sh
+++ serversetup/provision.sh
@@ -1,6 +1,5 @@
 pkg install -y git bash sudo tcl86 python35 mongodb rabbitmq py27-supervisor
 pkg install -y tcllib tdbc
-pkg install -y /home/bigcgi/bigcgi-repo/serversetup/apache24-2.4.26-suexec.txz
 python3.5 -m ensurepip
 pip3 install -r /home/bigcgi/bigcgi-repo/requirements.txt
 pw useradd bigcgi -d /home/bigcgi -m -s /usr/local/bin/bash
