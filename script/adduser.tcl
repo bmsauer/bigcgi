2a47d6181f696e8311a386a6a5534ad4391de7c1
Brian Sauer <brianmsauer@gmail.com>
2018-04-11 22:51:40 -0400

    server provisioning, user creation/cleanup on error cases

--- script/adduser.tcl
+++ script/adduser.tcl
@@ -19,6 +19,12 @@
 #adds a user from command line arguments.
 #this script should be run as sudo
 
+proc cleanup {username} {
+    if { [catch { exec "script/deluser.tcl" $username } msg ] } {
+	puts "Failed to cleanup user. $msg"
+    }
+}
+
 set username [lindex $argv 0]
 
 if {$tcl_platform(os) == "FreeBSD"} {
@@ -26,7 +32,7 @@ if {$tcl_platform(os) == "FreeBSD"} {
 } elseif {$tcl_platform(os) == "Linux"} {
     set command [list useradd -d /home/$username -m -s /bin/false $username]
 }  else {
-    puts "Could not detect os."
+    puts "Error could not detect os."
     exit 1
 }
 
@@ -36,31 +42,39 @@ if { [catch { exec {*}$command } msg] } {
 }
 if { [catch { exec mkdir /home/$username/public_html } msg ] } {
     puts "Error adding public_html directory: $msg"
+    cleanup  $username
     exit 2
 }
 if { [catch { exec chown $username:$username /home/$username/public_html } msg ] } {
     puts "Error changing public_html dir owner: $msg"
+    cleanup $username
     exit 2
 }
 if { [catch { exec mkdir /home/$username/files } msg ] } {
     puts "Error adding files directory: $msg"
+    cleanup $username
     exit 2
 }
 if { [catch { exec chown $username:$username /home/$username/files } msg ] } {
     puts "Error changing files dir owner: $msg"
+    cleanup $username
     exit 2
 }
 if { [catch { exec chmod -R 711 /home/$username } msg ] } {
     puts "Error changing home dir permissions: $msg"
+    cleanup $username
     exit 2
 }
 if { [catch { exec rctl -a user:$username:maxproc:deny=10/user } msg ] } {
     puts "Error setting process resource limits: $msg"
+    cleanup $username
     exit 2
 }
 if { [catch { exec rctl -a user:$username:memoryuse:deny=8M/user } msg ] } {
     puts "Error setting memory resource limits: $msg"
+    cleanup $username
     exit 2
 }
 
+puts "OK"
 exit 0
--- serversetup/provision.sh
+++ serversetup/provision.sh
@@ -1,7 +1,15 @@
-pkg install -y git bash sudo tcl86 python35 mongodb redis py27-supervisor apache24
+#!/bin/sh
+pkg install -y git bash sudo tcl86 python35 mongodb redis py27-supervisor apache24 wget unzip
 pkg install -y tcllib tdbc
 pw useradd bigcgi -d /home/bigcgi -m -s /usr/local/bin/bash
 python3.5 -m ensurepip
+#install cork from github repo
+wget https://github.com/FedericoCeratto/bottle-cork/archive/master.zip
+unzip master.zip
+cd bottle-cork-master
+python3.5 setup.py install
+cd ..
+rm -rf bottle-cork-master master.zip
 pip3 install -r /home/bigcgi/bigcgi-repo/requirements.txt
 . /home/bigcgi/bigcgi-repo/env.sh
 tclsh8.6 /home/bigcgi/bigcgi-repo/serversetup/install.tcl
